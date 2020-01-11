d5016cdf74a23045427d04bd26c26d57c0464a51
Piotr Zacha <pzacha11@gmail.com>
2020-01-11 10:21:21 +0100

    SQL database preparation

--- TODO/app.py
+++ TODO/app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
 
 @app.route('/')
 def home():
