a9fa8d17491ed1d178265cfb2c0511a77c958245
Piotr Zacha <pzacha11@gmail.com>
2020-01-15 21:09:24 +0100

    Added delete route

--- TODO/app.py
+++ TODO/app.py
@@ -11,22 +11,38 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200))
-    complete = db.Column(db.Boolean)
+    done = db.Column(db.Boolean)
+
+
+db.create_all()
 
 
 @app.route("/")
 def home():
-    todo_list = Todo.query.filter_by(complete=False).all()
-    done_list = Todo.query.filter_by(complete=True).all()
+
+    todo_list = Todo.query.filter_by(done=False).all()
+    done_list = Todo.query.filter_by(done=True).all()
 
     return render_template("home.html", todo_list=todo_list, done_list=done_list)
 
 
 @app.route("/add", methods=["POST"])
 def add():
-    todo = Todo(text=request.form["todoitem"], complete=False)
+
+    todo = Todo(text=request.form["todoitem"], done=False)
     db.session.add(todo)
     db.session.commit()
+
+    return redirect(url_for("home"))
+
+
+@app.route("/delete/<id>")
+def delete(id):
+
+    todo = Todo.query.get(id)
+    db.session.delete(todo)
+    db.session.commit()
+
     return redirect(url_for("home"))
 
 
@@ -34,7 +50,7 @@ def add():
 def done(id):
 
     todo = Todo.query.filter_by(id=int(id)).first()
-    todo.complete = True
+    todo.done = True
     db.session.commit()
 
     return redirect(url_for("home"))
